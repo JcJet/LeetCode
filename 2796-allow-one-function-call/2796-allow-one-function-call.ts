89f147b4776e7d02249ca7be48e83876ee8de330
Vladimir Andreev <jcjet88@gmail.com>
2023-06-26 11:07:45 +0300

    Time: 67 ms (31.81%), Space: 42.9 MB (50.39%) - LeetHub

---
+++ 2796-allow-one-function-call/2796-allow-one-function-call.ts
@@ -0,0 +1,25 @@
+type Fn = (...args: any[]) => any
+
+function once(fn: Fn): Fn {
+
+  let hasBeenCalled = false;
+  let result;
+
+  return (...args) => {
+    if (!hasBeenCalled) {
+      result = fn(...args);
+      hasBeenCalled = true;
+      return result;
+    } else {
+      return undefined;
+    }
+  };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
