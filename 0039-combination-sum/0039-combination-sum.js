72592844e29fcab8ad1d820f086d87afc220c8be
Vladimir Andreev <jcjet88@gmail.com>
2023-07-06 15:01:07 +0300

    Time: 64 ms (96.77%), Space: 44.9 MB (82.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -3,16 +3,25 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum = function(candidates, target) {
-    let res = [];
 
-    const dfs = (curCandidates, curTarget, value) => {
-        if( curTarget == 0)  res.push(value);
-        if(curTarget <= 0 ) return;
-        for(let g=0; g<curCandidates.length; g++){
-            dfs(curCandidates.slice(g), curTarget-curCandidates[g], [...value, curCandidates[g] ] );  
-        }
-    }
-    dfs(candidates, target, []);
-    return res;
+// Time O(2^t) where t is the target
+// Space O(2^t) where t is the target
+
+var combinationSum = function(candidates, target) {
+  const uniqueCombinations = [];
+  combinationHelper(0, [], 0, target, candidates, uniqueCombinations);   
+  return uniqueCombinations;
 };
+
+function combinationHelper(index, currentCombination, totalSum, target, candidates, uniqueCombinations) {
+  if (totalSum === target) {
+    uniqueCombinations.push(currentCombination.slice());
+    return;
+  }
+  if (index >= candidates.length || totalSum > target) return; 
+
+  currentCombination.push(candidates[index]);
+  combinationHelper(index, currentCombination, totalSum + candidates[index], target, candidates, uniqueCombinations);
+  currentCombination.pop();
+  combinationHelper(index + 1, currentCombination, totalSum, target, candidates, uniqueCombinations);
+}
