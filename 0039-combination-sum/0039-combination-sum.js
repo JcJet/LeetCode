3897a2b8fd2ce652c509e4dd6410f1810f8fa36f
Vladimir Andreev <jcjet88@gmail.com>
2023-07-06 15:00:01 +0300

    Time: 93 ms (17.28%), Space: 49.2 MB (13.32%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    let res = [];
+
+    const dfs = (curCandidates, curTarget, value) => {
+        if( curTarget == 0)  res.push(value);
+        if(curTarget <= 0 ) return;
+        for(let g=0; g<curCandidates.length; g++){
+            dfs(curCandidates.slice(g), curTarget-curCandidates[g], [...value, curCandidates[g] ] );  
+        }
+    }
+    dfs(candidates, target, []);
+    return res;
+};
