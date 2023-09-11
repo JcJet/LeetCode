f4b274ca40b26fb03079acd6e6fbd10dd13e0d24
Vladimir Andreev <jcjet88@gmail.com>
2023-09-11 13:09:27 +0300

    Time: 82 ms (80.65%), Space: 48.7 MB (61.29%) - LeetHub

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.ts
@@ -0,0 +1,18 @@
+function groupThePeople(groupSizes: number[]): number[][] {
+    const result: number[][] = [];
+    const groupSizesTable: Record<number, number[]> = {};
+
+    for (let i = 0; i < groupSizes.length; i++) {
+        const group: number = groupSizes[i]; 
+        if(groupSizesTable[group] === undefined) groupSizesTable[group] = [];
+
+        groupSizesTable[group].push(i);
+
+        if(groupSizesTable[group].length === group) {
+            result.push(groupSizesTable[group]);
+            delete groupSizesTable[group];
+        }
+    }
+
+    return result;
+};
