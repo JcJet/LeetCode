73f49ff083668b3010aa347e86e823903f31e71e
Vladimir Andreev <jcjet88@gmail.com>
2023-10-13 16:57:26 +0300

    Time: 16 ms (37.14%), Space: 19.1 MB (91.43%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.php
@@ -0,0 +1,14 @@
+class Solution {
+
+    /**
+     * @param Integer[] $cost
+     * @return Integer
+     */
+    function minCostClimbingStairs($cost) {
+        $cost[] = 0;
+        for ($i = count($cost) - 3; $i >= 0; $i--) {
+            $cost[$i] += min($cost[$i + 1], $cost[$i + 2]);
+        }
+        return min($cost[0], $cost[1]);
+    }
+    }
