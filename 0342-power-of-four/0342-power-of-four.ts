e9a7d7d87d702c3fd5fd6643d66d6e49499f58ba
Vladimir Andreev <jcjet88@gmail.com>
2023-10-23 17:09:24 +0300

    Time: 57 ms (79.49%), Space: 45 MB (26.5%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.ts
@@ -0,0 +1,10 @@
+function isPowerOfFour(n: number): boolean {
+    if (n === 0) return false;
+
+    while (n !== 1) {
+        if (n % 4) return false;
+        n = Math.floor(n / 4);
+    }
+
+    return true;
+}
