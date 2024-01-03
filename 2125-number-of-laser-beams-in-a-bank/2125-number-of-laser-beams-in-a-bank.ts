02cd9be6773222ce98e017b91a058359bc59ecf7
Vladimir Andreev <jcjet88@gmail.com>
2024-01-03 15:53:18 +0300

    Time: 70 ms (95.65%), Space: 52.7 MB (69.57%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.ts
@@ -0,0 +1,30 @@
+function countBank(bank: string): number {
+  let count = 0;
+  for (let i = 0; i < bank.length; i += 1) {
+    if (bank[i] === "1") {
+      count++;
+    }
+  }
+  return count;
+}
+
+function toNumbers(bank: string[]): number[] {
+  const result: number[] = [];
+  for (let i = 0; i < bank.length; i += 1) {
+    const count = countBank(bank[i]);
+    if (count) result.push(count);
+  }
+  return result;
+}
+
+function numberOfBeams(bank: string[]): number {
+  if (bank.length <= 1) return 0;
+
+  const numbers = toNumbers(bank);
+  let count = 0;
+  for (let i = 0; i < numbers.length - 1; i += 1) {
+    count += numbers[i] * numbers[i + 1];
+  }
+
+  return count;
+}
