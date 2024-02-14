6e05e45418ec4078be2ca4dcc170fdaf6faddeec
Vladimir Andreev <jcjet88@gmail.com>
2024-02-14 21:46:40 +0700

    Time: 330 ms (26.62%), Space: 96.2 MB (45.93%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+    const n = nums.length;
+    const output = new Array(n);
+    let i = 0, pos = 0, neg = 1;
+    
+    while (i < n) {
+        if (nums[i++] > 0) {
+            output[pos] = nums[i - 1];
+            pos += 2;
+        } else {
+            output[neg] = nums[i - 1];
+            neg += 2;
+        }
+    }
+    
+    return output;
+};
