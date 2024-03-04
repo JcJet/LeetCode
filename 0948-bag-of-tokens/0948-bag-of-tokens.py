5fe70c65e1ce1206e337d472497872887e2c4023
Vladimir Andreev <jcjet88@gmail.com>
2024-03-04 17:21:18 +0700

    Time: 57 ms (34.15%), Space: 16.6 MB (94.31%) - LeetHub

---
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -0,0 +1,41 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        if not tokens:
+            return 0
+
+        self.quicksort(tokens, 0, len(tokens) - 1)
+        left, score, maxScore, right = 0, 0, 0, len(tokens) - 1
+
+        while left <= right:
+            if power >= tokens[left]:
+                power -= tokens[left]
+                score += 1
+                left += 1
+            elif score > 0:
+                power += tokens[right]
+                score -= 1
+                right -= 1
+            else:
+                break
+
+            maxScore = max(score, maxScore)
+
+        return maxScore
+
+    def quicksort(self, arr, left, right):
+        if left < right:
+            pivotIndex = self.partition(arr, left, right)
+            self.quicksort(arr, left, pivotIndex - 1)
+            self.quicksort(arr, pivotIndex + 1, right)
+
+    def partition(self, arr, left, right):
+        pivotValue = arr[right]
+        i = left - 1
+
+        for j in range(left, right):
+            if arr[j] < pivotValue:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+
+        arr[i + 1], arr[right] = arr[right], arr[i + 1]
+        return i + 1
