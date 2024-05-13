00c0284e9e888e809b72ecd28db20fc6973f1e12
Vladimir Andreev <jcjet88@gmail.com>
2024-05-13 07:31:44 +0300

    Time: 71 ms (50%), Space: 51.5 MB (100%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.ts
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.ts
@@ -1,32 +1,31 @@
-function matrixScore(grid: number[][]): number {
-    const rows: number = grid.length;
-        const cols: number = grid[0].length;
-        
-        // Toggle rows
-        for (let r = 0; r < rows; r++) {
-            if (grid[r][0] === 0) {
-                for (let c = 0; c < cols; c++) {
-                    grid[r][c] = 1 - grid[r][c];
-                }
+const matrixScore = (grid: number[][]): number => {
+    const N = grid.length;
+    const M = grid[0].length;
+
+    if (N === 1 && M === 1) return 1;
+
+    // Flip rows to maximise most significant digit
+    for (let i = 0; i < N; i++) {
+        if (grid[i][0] === 0) {
+            for (let j = 0; j < M; j++) {
+                grid[i][j] = (grid[i][j] + 1) % 2;
             }
         }
-        
-        // Count zeroes in columns
-        const counts: Map<number, number> = new Map();
-        for (let c = 1; c < cols; c++) {
-            for (let r = 0; r < rows; r++) {
-                if (grid[r][c] === 0) {
-                    counts.set(c, (counts.get(c) || 0) + 1);
-                }
+    }
+
+    // flip columns if we create more 1s instead of 0s
+    for (let i = 1; i < M; i++) {
+        let countOnes = grid.map((row) => row[i]).reduce((p, v) => p + v, 0);
+        if (countOnes < N - countOnes) {
+            for (let j = 0; j < N; j++) {
+                grid[j][i] = (grid[j][i] + 1) % 2;
             }
         }
-        
-        // Calculate score
-        let res: number = rows * Math.pow(2, cols - 1);
-        for (let c = 1; c < cols; c++) {
-            const count: number = counts.get(c) || 0;
-            res += Math.max(count, rows - count) * Math.pow(2, cols - c - 1);
-        }
-        
-        return res;
+    }
+
+    const binaries = grid.map((row: number[]) =>
+        row.reduce((p, v, i) => p + (v << (M - i - 1)), 0)
+    );
+
+    return binaries.reduce((p, v) => p + v, 0);
 };
