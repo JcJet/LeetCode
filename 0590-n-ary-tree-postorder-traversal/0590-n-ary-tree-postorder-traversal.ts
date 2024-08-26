96654d5c7c8003a7226aee02e709c8a88e3420c9
Vladimir Andreev <jcjet88@gmail.com>
2024-08-26 15:09:07 +0700

    Time: 103 ms (7.79%), Space: 55.1 MB (72.73%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.ts
@@ -0,0 +1,30 @@
+
+// Definition for node.
+class NodeWithChildren {
+    val: number
+    children: NodeWithChildren[]
+    constructor(val?: number) {
+        this.val = (val === undefined ? 0 : val)
+        this.children = []
+    }
+}
+
+function postorder(root: NodeWithChildren | null): number[] {
+
+    if (!root) return [];
+
+    const stack = [root]
+    const results = []
+
+    // DFS 
+    while (stack.length) {
+        const currNode = stack.pop()
+
+        if (currNode.children.length > 0) {
+            stack.push(...currNode.children);
+        }
+        results.push(currNode.val)
+    }
+
+    return results.reverse();
+};
