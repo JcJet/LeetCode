6b11c5f45ff887d3c0706145b609683b9cd388ac
Vladimir Andreev <jcjet88@gmail.com>
2024-10-06 23:03:21 +0300

    Time: 70 ms (20%), Space: 51.3 MB (80%) - LeetHub

---
+++ 0006-sentence-similarity-iii/0006-sentence-similarity-iii.ts
@@ -0,0 +1,38 @@
+function areSentencesSimilar(sentence1: string, sentence2: string): boolean {
+    
+    const arr1 = sentence1.split(" ")
+    const arr2 = sentence2.split(" ")
+    // Converted strings to arrays
+    // Example: "a b c" => ["a", "b", "c"]
+
+    let commonPreLength = 0
+    let commonSuffixLength = 0
+
+    for(let i = 0;;i++){
+        // Counting from start 
+        
+        const [element1, element2] = [arr1[i], arr2[i]]
+        if(element1==null || element2==null) return true
+        // They are totally equal
+        
+        if(element1 === element2) commonPreLength++
+        // Accumulator of equal words
+
+        else break // We found not matching words, we are done here
+    }
+
+    for(let i = 0;;i++){
+        // Same but for suffix
+        const [element1, element2] = [arr1[arr1.length-1-i], arr2[arr2.length-1- i]]
+        if(element1==null || element2==null) return true
+        if(element1 === element2) commonSuffixLength++
+        else break 
+    }
+
+    const equalOuter = commonPreLength + commonSuffixLength
+
+    if(equalOuter>= arr1.length || equalOuter>= arr2.length) return true
+    // If sum was more than the length of the arrays, we return true
+
+    return false    
+};
